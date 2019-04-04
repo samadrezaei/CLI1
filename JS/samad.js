b581acca324dd39359044b815c52ceff4765b026
Samad-Rezaei <46791571+sghu4me@users.noreply.github.com>
2019-04-04 19:48:49 +0300

    Add files via upload

---
+++ JS/samad.js
@@ -0,0 +1,43 @@
+// let box = 'green maker';
+// let boxsmall = "red marker";
+// let boxLarge = 33;
+// let boxRegular = false;
+// let boxhumungus = {
+// boxTiny = 'yes' ,
+// boxRegular : true ,
+// innerObject : {
+//     thirdObject : {
+//         anotherVariable : 'enough'
+//     }
+// }
+// };
+// let captain = [];
+// let popObject = {};
+// let syntaxarray = [
+//     31,
+//     'mystring',
+//     false,
+//     {},
+//     {
+//         myarray : [
+
+//         ],
+//         NewObject : {}
+//     }
+// ];
+// let name = "samad ";
+// let greeting = "Hello, my name's";
+// let age = 31;
+// greeting + " " +name+ "im " +age+ " years old"
+
+
+// let start = 'start at';
+// let nex = 0;
+// start + next;
+let count = 'counting ';
+count += 1;
+count += 2;
+count += 3 + 4 + 5;
+console.log(count);
+console.log(typeof count=='string');
+console.log(count.length);
